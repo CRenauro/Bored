e8044b7267dd11b3f29a4e421013b892f97e6170
Hiral patel <hiralm227@gmail.com>
2021-03-26 15:07:33 -0400

    update

--- public/assets/js/post.js
+++ public/assets/js/post.js
@@ -1,9 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
+const searchButton = document.getElementById("searchInput")
 
-    const home = fetch("/", {
-        method: "GET",
-        headers: { "Content-Type": "application/json" }
-    })
+async function getFish() {
 
+    // debugger;
+    console.log("getFish...");
+    try {
+        // Get all posts
+        // const response = await fetch("/api/post");
 
+        // Grab search box input value here
+        // For now, we temporarily assign "1" to a const variable 
+        const searchInput = "1"; 
+        const URL = "/api/post" + "/" + searchInput;
+        console.log("getFish - url: " + URL);
+
+        const response = await fetch(URL);
+        console.log(response);
+
+        const data = await response.json();
+        console.log("/api/post - data:", data);
+
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+
+searchButton.addEventListener("click", function(e) {
+    e.preventDefault()
+    console.log("Running function ");
+    getFish()
 })
